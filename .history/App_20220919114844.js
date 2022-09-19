525f7585eb93487bd595f6056a0194e7216e8000
Peace Akinola <akinolapo@gmail.com>
2022-09-19 17:39:30 +0100

    dependencies installed

--- .history/App_20220919114844.js
+++ .history/App_20220919114844.js
@@ -1,15 +1,20 @@
 // import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import {createStackNavigator} from '@react-navigation/stack'
-import {NavigationContainer, DefaultTheme} from '@react-navigation/native'
 
 export default function App() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Hello World!</Text>
       {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
